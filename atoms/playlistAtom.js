30e27209049eef28ec8aff4bb1d39978e84a5094
Yahya <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-11-30 18:10:40 +0100

    Sprints #1 & #2

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null,
+})
+
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: '4fwz0JpsyLNu6Oa4t8ABCj'
+});
